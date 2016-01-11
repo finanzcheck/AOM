63fa6ac484d03afd94d878bf4bf1bd5978e43343
Daniel Stonies <daniel.stonies@googlemail.com>
2016-01-11 18:49:10 +0100

    Add id for Bing and Facebook

--- Platforms/FacebookAds/Installer.php
+++ Platforms/FacebookAds/Installer.php
@@ -16,6 +16,7 @@ class Installer implements InstallerInterface
     {
         try {
             $sql = 'CREATE TABLE ' . FacebookAds::getDataTableNameStatic() . ' (
+                        id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
                         id_account_internal VARCHAR(50) NOT NULL,
                         idsite INTEGER NOT NULL,
                         date DATE NOT NULL,
