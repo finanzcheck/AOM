70ecd6c0a023692f511d2cd5616ebb750a5a9482
Daniel Stonies <daniel.stonies@googlemail.com>
2016-05-22 15:08:17 +0200

    Increase memory limit

--- Commands/PlatformMerge.php
+++ Commands/PlatformMerge.php
@@ -49,7 +49,7 @@ class PlatformMerge extends ConsoleCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // We might need a little more RAM
-        ini_set('memory_limit','512M');
+        ini_set('memory_limit','1024M');
 
         if (!in_array($input->getOption('platform'), AOM::getPlatforms())) {
             $this->logger->warning('Platform "' . $input->getOption('platform') . '" is not supported.');
